1ce812a1ae3207cd7450261e72a2e6f31dd15137
Simon Forsling <simon.forsling@gmail.com>
2018-10-20 22:59:59 +0200

    Support for existing swap upon open

--- extension.js
+++ extension.js
@@ -1,28 +1,52 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
-//const fs = require('fs');
+const fs = require('fs');
+
+function fileExists(doesIt) {
+    if (doesIt) {
+
+    }
+}
 
 function activate(context) {
     let currentWatcher = vscode.workspace.createFileSystemWatcher(new vscode.RelativePattern("!", "!")); 
-    let channel = vscode.window.createOutputChannel('relative');
+    let channel = vscode.window.createOutputChannel('swap');
     context.subscriptions.push(channel);
 
-    channel.appendLine(`New output channel`);
+    //channel.appendLine(`New output channel`);
 
-    vscode.workspace.onDidOpenTextDocument(doc => { 
+    /*vscode.workspace.onDidOpenTextDocument(doc => { 
         console.log('OPENED => ' + doc.uri.toString(true));
     });
 
     vscode.workspace.onDidCloseTextDocument(doc => { 
         console.log('CLOSED => ' + doc.uri.toString(true));
-    });
+    }); */
 
     vscode.window.onDidChangeActiveTextEditor(e => { 
+        console.log(e.document.uri);
         var documentPath = e.document.uri.fsPath;
-        var currentPath = documentPath.substring((0), documentPath.lastIndexOf('\\') + 1);
-        var currentFile = documentPath.substring(documentPath.lastIndexOf('\\') + 1, documentPath.length - 1);
+
+        var lastSlashIndex = documentPath.lastIndexOf('/');
+        if (lastSlashIndex === -1) {
+            documentPath.lastIndexOf('\\')
+        }
+
+        var currentPath = documentPath.substring((0), lastSlashIndex + 1);
+        var currentFile = documentPath.substring(lastSlashIndex + 1, documentPath.length);
         var currentFileName = currentFile.substring(0, currentFile.lastIndexOf('.'));
+
+        var targetPath = currentPath + currentFileName + '.swp';
+        console.log("TargetPath: " + targetPath);
+        if (fs.existsSync(targetPath)) {
+            vscode.window.showErrorMessage(currentFileName + " is being edited in vim!");
+            channel.appendLine("No .swp file found for file " + currentFile);
+            return;
+        }
+        else {
+            channel.appendLine("No .swp file found for file " + currentFile);
+        }
 
         let pattern = new vscode.RelativePattern(currentPath, (currentFileName + '.swp'));
         console.log(pattern);
