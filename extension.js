906aff9522ca704f1be819d4c1c1415e101fbc82
Simon Forsling <simon.forsling@gmail.com>
2018-10-21 01:38:48 +0200

    Guard against undefined document

--- extension.js
+++ extension.js
@@ -8,38 +8,41 @@ let currentWatcher;
 function activate(context) {
 
     let listener = vscode.window.onDidChangeActiveTextEditor(e => { 
-        //console.log(e.document.uri);
-        var documentPath = e.document.uri.fsPath;
+        if (e.document) {
+            //console.log(e.document.uri);
+            if (e.document) { }
+            var documentPath = e.document.uri.fsPath;
 
-        var lastSlashIndex = documentPath.lastIndexOf('/');
-        if (lastSlashIndex === -1) {
-            lastSlashIndex = documentPath.lastIndexOf('\\')
+            var lastSlashIndex = documentPath.lastIndexOf('/');
+            if (lastSlashIndex === -1) {
+                lastSlashIndex = documentPath.lastIndexOf('\\')
+            }
+
+            var currentPath = documentPath.substring((0), lastSlashIndex + 1);
+            var currentFile = documentPath.substring(lastSlashIndex + 1, documentPath.length);
+            var currentFileName = currentFile.substring(0, currentFile.lastIndexOf('.'));
+
+            var targetPath = currentPath + currentFileName + '.swp';
+            if (fs.existsSync(targetPath)) {
+                vscode.window.showErrorMessage(currentFileName + " is being edited in vim!");
+                //return;
+            }
+
+            let pattern = new vscode.RelativePattern(currentPath, (currentFileName + '.swp'));
+            //console.log(pattern);
+
+            if (currentWatcher) { currentWatcher.dispose() }
+            currentWatcher = vscode.workspace.createFileSystemWatcher(pattern);
+            currentWatcher.onDidCreate( () => {
+                vscode.window.showErrorMessage(currentFileName + " is being edited in vim!", 'Open Dialog', 'Save As Dialog')
+                .then((choice) => showDialog(choice));
+            });
+
+            currentWatcher.onDidDelete(() => {
+                vscode.window.showWarningMessage('This file is no longer being edited, but may have new changes', 'Open Dialog', 'Save As Dialog')
+                .then((choice) => showDialog(choice));
+            });
         }
-
-        var currentPath = documentPath.substring((0), lastSlashIndex + 1);
-        var currentFile = documentPath.substring(lastSlashIndex + 1, documentPath.length);
-        var currentFileName = currentFile.substring(0, currentFile.lastIndexOf('.'));
-
-        var targetPath = currentPath + currentFileName + '.swp';
-        if (fs.existsSync(targetPath)) {
-            vscode.window.showErrorMessage(currentFileName + " is being edited in vim!");
-            //return;
-        }
-
-        let pattern = new vscode.RelativePattern(currentPath, (currentFileName + '.swp'));
-        //console.log(pattern);
-
-        if (currentWatcher) { currentWatcher.dispose() }
-        currentWatcher = vscode.workspace.createFileSystemWatcher(pattern);
-        currentWatcher.onDidCreate( () => {
-            vscode.window.showErrorMessage(currentFileName + " is being edited in vim!", 'Open Dialog', 'Save As Dialog')
-            .then((choice) => showDialog(choice));
-        });
-
-        currentWatcher.onDidDelete(() => {
-            vscode.window.showWarningMessage('This file is no longer being edited, but may have new changes', 'Open Dialog', 'Save As Dialog')
-            .then((choice) => showDialog(choice));
-        });
     });
     context.subscriptions.push(listener);
     
