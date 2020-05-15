19b3fcc3f34c225cc3c6918fb0f92f9bfea2bcc5
Simon Forsling <simon.forsling@gmail.com>
2020-05-15 17:30:58 +0200

    Cleanup extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -1,16 +1,22 @@
 import * as vscode from 'vscode';
+import * as fs from 'fs';
+
+import * as ds from './display';
 import { DsDocument, swpFile, DsDocArray } from './types';
 import { checkSwp, tryLockFile } from './filetasks';
-import * as ds from './display';
-import * as fs from 'fs';
+
 
 const swpString: string = "VSCODE/" + vscode.env.machineId;
 let fullSwpString: string = swpString;
+
 let DsDocs: DsDocArray = {};
 let swpStatusBar: vscode.StatusBarItem;
 let active: boolean = true;
 
 export function activate(context: vscode.ExtensionContext) {
+	/**********************
+	*  Handle settings
+	***********************/
 	let swpName: string = vscode.workspace.getConfiguration().get('dirtyswp.writeNameToSwp') || "";
 	if (typeof swpName != 'undefined' && swpName) {
 		fullSwpString = swpString + ":" + swpName;
@@ -19,21 +25,25 @@ export function activate(context: vscode.ExtensionContext) {
 	active = vscode.workspace.getConfiguration().get('dirtyswp.startActive') || true;
 	let statusBarEnabled = vscode.workspace.getConfiguration().get('dirtyswp.showStatusBarItem');
 
+	/**********************
+	*  Listeners
+	***********************/
 	let openDocumentListener = vscode.workspace.onDidOpenTextDocument(e => {
 		if (!active || e.uri.scheme != "file") {
 			return;
 		}
 
-		//create DocumentInfo object and add it to documents
+		//Create DocumentInfo object and add it to documents
 		let dsDoc = new DsDocument(e);
 		DsDocs[e.uri.toString()] = dsDoc;
 
-		//warn user if file is being edited somewhere else
+		//Warn user if file is being edited somewhere else
 		checkSwp(dsDoc, (swp) => {
 			ds.warn(dsDoc.basename, false, swp);
 		}, () => { });
 		console.log(DsDocs);
 	});
+	context.subscriptions.push(openDocumentListener);
 
 	let closeDocumentListener = vscode.workspace.onDidCloseTextDocument(e => {
 		if (!active || e.uri.scheme != "file") {
@@ -47,6 +57,7 @@ export function activate(context: vscode.ExtensionContext) {
 		delete DsDocs[e.uri.toString()];
 		console.log(DsDocs);
 	});
+	context.subscriptions.push(closeDocumentListener);
 
 	let documentChangedListener = vscode.workspace.onDidChangeTextDocument(e => {
 		if (!active || e.document.uri.scheme != "file") {
@@ -55,24 +66,24 @@ export function activate(context: vscode.ExtensionContext) {
 
 		let test = e.document.uri.toString();
 		let doc = DsDocs[e.document.uri.toString()];
-		if (!doc.textDocument.isDirty) { //file has no unsaved changes
+		if (!doc.textDocument.isDirty) {
 			doc.potentialUnsyncedChanges = false;
+			//If file is no longer dirty, but still has our swp, then we can remove the .swp file (unless 'lock until close' is set)
 			if (doc.hasOurSwp && !doc.forceLock) {
-				//if file is no longer dirty but still has our swp, then we can remove the .swp file (unless 'lock until close' is set)
 				doc.removeOwnSwp();
 			}
 		}
-		else if (!doc.hasOurSwp) { //file has unsaved changes but is not locked by us
+		else if (!doc.hasOurSwp) { //File has unsaved changes but is not locked by us
 			checkSwp(doc, (swp) => {
 				doc.potentialUnsyncedChanges = true;
 				ds.warn(doc.basename, true, swp);
 			}, () => {
 				if (doc.potentialUnsyncedChanges) {
-					//even if the file is no longer in use by someone else, there may still be changes that have not been loaded (since file is dirty)
+					//Even if the file is no longer in use by someone else, there may still be changes that have not been loaded (since file is dirty)
 					vscode.window.showWarningMessage(doc.basename + " is no longer being edited elsewhere but may have unsynced changes: Save may overwrite changes!", 'Open Dialog', 'Save As Dialog')
 						.then((choice) => ds.showDialog(choice));
 				} else {
-					//if the file is not currently locked and has no potential unloaded changes, then we may lock the file for ourselves
+					//If the file is not currently locked and has no potential unloaded changes, then we may lock the file for ourselves
 					try {
 						fs.writeFileSync(doc.swapPath, fullSwpString);
 						doc.hasOurSwp = true;
@@ -83,6 +94,7 @@ export function activate(context: vscode.ExtensionContext) {
 			});
 		}
 	});
+	context.subscriptions.push(documentChangedListener);
 
 	let confChangedListener = vscode.workspace.onDidChangeConfiguration((e) => {
 		let statusbarAffected = e.affectsConfiguration('dirtyswp.showStatusBarItem');
@@ -96,15 +108,11 @@ export function activate(context: vscode.ExtensionContext) {
 		}
 	});
 	context.subscriptions.push(confChangedListener);
-
-	//on startup, add any already opened documents
-	addOpenDocuments();
-
-	context.subscriptions.push(openDocumentListener);
-	context.subscriptions.push(closeDocumentListener);
-	context.subscriptions.push(documentChangedListener);
-
-	context.subscriptions.push(vscode.commands.registerCommand('dirtyswp.start', () => {
+	
+	/**********************
+	*  Register commands
+	***********************/
+	let startCmd = vscode.commands.registerCommand('dirtyswp.start', () => {
 		if (active) {
 			vscode.window.showInformationMessage("Dirty.swp is already active");
 			return
@@ -112,18 +120,20 @@ export function activate(context: vscode.ExtensionContext) {
 		vscode.window.showInformationMessage("Resuming .swp monitoring and locking");
 		active = true;
 		addOpenDocuments(true);
-	}));
+	});
+	context.subscriptions.push(startCmd);
 
-	context.subscriptions.push(vscode.commands.registerCommand('dirtyswp.stop', () => {
+	let stopCmd = vscode.commands.registerCommand('dirtyswp.stop', () => {
 		if (!active) {
 			vscode.window.showInformationMessage("Dirty.swp is already paused");
 			return
 		}
 		vscode.window.showInformationMessage("Pausing .swp monitoring and locking.");
 		deactivate();
-	}));
+	});
+	context.subscriptions.push(stopCmd);
 
-	context.subscriptions.push(vscode.commands.registerCommand('dirtyswp.lockuntilclose', () => {
+	let lockCmd = vscode.commands.registerCommand('dirtyswp.lockuntilclose', () => {
 		if (typeof vscode.window.activeTextEditor === 'undefined') {
 			return;
 		}
@@ -131,18 +141,27 @@ export function activate(context: vscode.ExtensionContext) {
 		let dsDoc = DsDocs[name];
 		dsDoc.forceLock = true;
 		tryLockFile(dsDoc)
-	}));
+	});
+	context.subscriptions.push(lockCmd);
 
-	context.subscriptions.push(vscode.commands.registerCommand('dirtyswp.listswp', ds.listSwp));
+	let listCmd = vscode.commands.registerCommand('dirtyswp.listswp', ds.listSwp);
+	context.subscriptions.push(listCmd);
 
+	/**********************
+	*  Other stuff
+	***********************/
+
+	//Add any documents open on start
+	addOpenDocuments();
+
+	//Create status bar
 	swpStatusBar = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 1);
-    context.subscriptions.push(swpStatusBar);
-    swpStatusBar.text = ".swp";
-    swpStatusBar.command = "dirtyswp.listswp";
-    if (statusBarEnabled) {
-        swpStatusBar.show();
-    }
-    context.subscriptions.push(swpStatusBar);
+	swpStatusBar.text = ".swp";
+	swpStatusBar.command = "dirtyswp.listswp";
+	if (statusBarEnabled) {
+		swpStatusBar.show();
+	}
+	context.subscriptions.push(swpStatusBar);
 }
 
 function addOpenDocuments(createSwpIfDirty = false) {
@@ -164,7 +183,6 @@ function addOpenDocuments(createSwpIfDirty = false) {
     })
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() { 
 	for (let doc of Object.keys(DsDocs)) {
         DsDocs[doc].removeOwnSwp();
